ff2aafea207ac3505027a494524afa29c131b18e
Laalitha <laalithagajanayake@gmail.com>
2024-09-20 17:38:39 +0530

    Compiling Typescript

---
+++ TypeScript/Compiling_TypeScript/sandbox.ts
@@ -0,0 +1,11 @@
+const character = 'Aaron';
+
+console.log(character);
+
+const inputEls = document.querySelectorAll('input');
+
+console.log(inputEls);
+
+inputEls.forEach(inputEl => {
+    console.log(inputEl);
+})

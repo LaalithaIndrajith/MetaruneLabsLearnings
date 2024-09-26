101b0abcb0dec3c5f3f1531398b3eaca79fcb610
Laalitha <laalithagajanayake@gmail.com>
2024-09-26 13:47:43 +0530

    Initial commit for Shopping Cart Project

---
+++ React/Shopping-Cart/src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            Home
+          </Route>
+          <Route path="/store" element={<Store />}>
+            Store
+          </Route>
+          <Route path="/about" element={<About />}>
+            About
+          </Route>
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
